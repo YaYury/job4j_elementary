eff37f89609562ca050a545f940afd4e32441f2b
YaYury <yavorsin13@gmail.com>
2020-11-01 21:15:49 +0300

    3.3. Разбор первой программы. [#393467]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,7 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String [] args)  {
+        System.out.println ("Yuri Vorsin");
+        System.out.println ("11.05.2021");}
+}
