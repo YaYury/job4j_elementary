2edc99fc62ec1983fffdc7e699572acc5cf6bb0f
YaYury <yavorsin13@gmail.com>
2020-11-27 21:27:11 +0300

    4.3.2. Оператор || [#393401]

---
+++ src/ru/job4j/condition/OR.java
@@ -0,0 +1,22 @@
+package ru.job4j.condition;
+
+public class OR {
+    public static void main(String[] args) {
+        boolean first = true;
+        boolean second = true;
+        boolean join = first || second;
+        System.out.println(join);
+        first = true;
+        second = false;
+        join = first || second;
+        System.out.println(join);
+        first = false;
+        second = true;
+        join = first || second;
+        System.out.println(join);
+        first = false;
+        second = false;
+        join = first || second;
+        System.out.println(join);
+    }
+}
