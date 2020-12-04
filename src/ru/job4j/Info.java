18ae16f818c9e284422aeb7e9ca279b907c48454
YaYury <yavorsin13@gmail.com>
2020-12-04 23:14:21 +0300

    5.8. Именование классов и переменных [#393515]

--- src/ru/job4j/Info.java
+++ src/ru/job4j/Info.java
@@ -1,6 +1,7 @@
 package ru.job4j;
 
 public class Info {
-    public static void main(String [] args)  {
-        System.out.println ("03.11.2020");}
+    public static void main(String[] args) {
+        System.out.println("03.11.2020");
+    }
 }
