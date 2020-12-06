1afc0289ef601bd6d144a4da12c2ac459dbf160d
YaYury <yavorsin13@gmail.com>
2020-12-06 13:37:42 +0300

    5.7. Ипотека [#393506]

--- src/ru/job4j/loop/Mortgage.java
+++ src/ru/job4j/loop/Mortgage.java
@@ -4,9 +4,8 @@ public class Mortgage {
     public static int year(int amount, int salary, double percent) {
         int year = 0;
         percent = percent / 100;
-        double residue = amount;
-        while (residue > 0) {
-                residue = residue + (residue * percent) - salary;
+        while (amount > 0) {
+                amount = (int) (amount + (amount * percent) - salary);
                 year += 1;
             }
         return year;
